48624996b8b7eb3761f2c31eaa0c701d31634bcb
ChrisKnowsDev <chrisharley81@gmail.com>
2020-08-25 12:08:49 -0400

    installed react and react-dom

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Pet from './Pet';
+
+export default function App() {
+  return (
+    <div>
+      <h1>hello from app</h1>
+    </div>
+  );
+}
