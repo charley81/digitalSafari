3c8e0da12298cbebc9df3763163363b47338a917
ChrisKnowsDev <chrisharley81@gmail.com>
2020-08-25 12:27:50 -0400

    initial setup complete

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,12 @@ import Pet from './Pet';
 export default function App() {
   return (
     <div>
-      <h1>hello from app</h1>
+      <h1 id='logo'>digitalSafari</h1>
+      <Pet name='Chance' animal='dog' breed='unknown' />
+      <Pet name='Pepper' animal='bird' breed='Cardinal' />
+      <Pet name='Cleo' animal='cat' breed='Calico' />
     </div>
   );
 }
+
+render(React.createElement(App), document.getElementById('root'));
