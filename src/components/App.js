ecdc02628048ffc1ae24436057b9b0b6dd5767c7
ChrisKnowsDev <chrisharley81@gmail.com>
2020-08-28 17:29:53 -0400

    api data fetch, main pet cards showing upon search

--- src/components/App.js
+++ src/components/App.js
@@ -4,18 +4,20 @@ import SearchParams from './SearchParams';
 
 export default function App() {
   return (
-    <div>
-      <header>
-        <h1 className='logo'>
-          digital<span>Safari</span>{' '}
-        </h1>
-        <p>
-          This is a test site... Data is limited to Seattle, WA & San Francisco,
-          CA
-        </p>
-      </header>
-      <SearchParams />
-    </div>
+    <React.StrictMode>
+      <div>
+        <header>
+          <h1 className='logo'>
+            digital<span>Safari</span>{' '}
+          </h1>
+          <p>
+            This is a test site... Data is limited to Seattle, WA & San
+            Francisco, CA
+          </p>
+        </header>
+        <SearchParams />
+      </div>
+    </React.StrictMode>
   );
 }
 
